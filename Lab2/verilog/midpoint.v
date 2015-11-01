086032f7ef118f11ee1f6d10c58d15a79b499bc2
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-11-01 00:22:47 -0400

    fix bugs

--- Lab2/verilog/midpoint.v
+++ Lab2/verilog/midpoint.v
@@ -14,7 +14,7 @@ module midpoint(
 
 	inputconditioner cond0(clk, btn[0], conditioned[0], rising[0], falling[0]);
 	inputconditioner cond1(clk, sw[0], conditioned[1], rising[1], falling[1]);
-	inputconditioner cond2(clk, sw[0], conditioned[2], rising[2], falling[2]);
+	inputconditioner cond2(clk, sw[1], conditioned[2], rising[2], falling[2]);
 
 	shiftregister shiftreg(clk, rising[2], falling[0], 8'hA5, conditioned[1], parallelOut, serialOut);
 endmodule
