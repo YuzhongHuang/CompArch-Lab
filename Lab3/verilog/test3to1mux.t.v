752d8316d074e09dc2ace5d1aa0c7aeba52ab970
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2015-11-16 12:50:08 -0500

    Completed 3to1mux testbench

--- Lab3/verilog/test3to1mux.t.v
+++ Lab3/verilog/test3to1mux.t.v
@@ -1,3 +1,5 @@
+`include "muxes.v"
+
 module test3to1mux();	
 	wire[31:0]    	out;	
 	wire[1:0]	address;
@@ -6,22 +8,75 @@ module test3to1mux();
 	reg		begintest;
 	wire		dutpassed;
 
-mux3to1 dut2(.out(out),
+	mux3to1 dut2(.out(out),
 			.address(address),
 			.in0(in0),
 			.in1(in1),
 			.in2(in2));
+
+	mux3to1testbench test(.begintest(begintest),
+							.endtest(endtest),
+							.dutpassed(dutpassed),
+							.address(address),
+							.in0(in0),
+							.in1(in1),
+							.in2(in2),
+							.out(out));
+
 	initial begin
-	// $display("Testing 3:1 Mux:");
-	// $display("in0  in1  in3| address | out | expected out"); // Prints header for truth table
-	in0=32'd126; in1=32'd53; in2=32'd178; address=2'b00; #1 // Set inputs and address, wait for new update
-	// $display("%d  %d  %d|  %d  |  %d | 126", in0, in1, in2, address, out);
-	in0=32'd135; in1=32'd97; in2=32'd14; address=2'b01; #1
-	// $display("%d  %d  %d|  %d  |  %d | 97", in0, in1, in2, address, out);
-	in0=32'd49; in1=32'd299; in2=32'd28; address=2'b10; #1
-	// $display("%d  %d  %d|  %d  |  %d | 28", in0, in1, in2, address, out);
-	end
+        begintest = 0;
+        #10;
+        begintest = 1;
+        #10000;
+    end
+
+    always @(posedge endtest) begin
+        if (dutpassed == 1) begin
+            $display("\n\033[32mDUT passed: %b\033[37m\n", dutpassed);
+        end else begin
+            $display("\n\033[31mDUT passed: %b\033[37m\n", dutpassed);
+        end
+    end
+	
 endmodule
 
-module 3to1testbench
-(
+module mux3to1testbench (
+	input begintest,
+	output reg endtest,
+	output reg dutpassed,
+
+	output reg [1:0] address,
+	output reg [31:0] in0, in1, in2,
+	input [31:0] out
+);
+
+	always @(posedge begintest) begin
+		endtest = 0;
+		dutpassed = 1;
+
+		// Test Case 1 (picks in0)
+		in0=32'd126; in1=32'd53; in2=32'd178; address=0; #1
+		if (out != in0) begin
+			dutpassed = 0;
+			$display("Select in0 broken.");
+		end
+
+		// Test Case 2 (picks in1)
+		address=1; #1
+		if (out != in1) begin
+			dutpassed = 0;
+			$display("Select in0 broken.");
+		end
+
+		// Test Case 3 (picks in2)
+		address=2; #1
+		if (out != in2) begin
+			dutpassed = 0;
+			$display("Select in0 broken.");
+		end
+
+		endtest = 1;
+		$finish;
+	end
+
+endmodule
