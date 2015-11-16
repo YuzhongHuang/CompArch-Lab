1754424e53736c09a32c48e99467d488e9231844
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2015-11-16 12:05:25 -0500

    uncommented mux file

--- Lab3/verilog/testmuxes.t.v
+++ Lab3/verilog/testmuxes.t.v
@@ -22,41 +22,41 @@ module testMuxes();
 	$display("%d  %d |  %d  |  %d | 3", in0, in1, address1, out);
 	end
 
-	// mux3to1 dut2(.out(out),
-	// 		.address(address),
-	// 		.in0(in0),
-	// 		.in1(in1),
-	// 		.in2(in2));
+	mux3to1 dut2(.out(out),
+			.address(address),
+			.in0(in0),
+			.in1(in1),
+			.in2(in2));
 
-	// initial begin
-	// $display("Testing 3:1 Mux:");
-	// $display("in0  in1  in3| address | out | expected out"); // Prints header for truth table
-	// in0=32'b1; in1=32'b0; in2=32'b0; address=2'b00; #1 // Set inputs and address, wait for new update
-	// $display("%b  %b  %b|  %b  |  %b | 1", in0, in1, in2, address, out);
-	// in0=32'b0; in1=32'b1; in2=32'b0; address=2'b01; #1
-	// $display("%b  %b  %b|  %b  |  %b | 1", in0, in1, in2, address, out);
-	// in0=32'b0; in1=32'b0; in2=32'b1; address=2'b10; #1
-	// $display("%b  %b  %b|  %b  |  %b | 1", in0, in1, in2, address, out);
-	// end
+	initial begin
+	$display("Testing 3:1 Mux:");
+	$display("in0  in1  in3| address | out | expected out"); // Prints header for truth table
+	in0=32'b1; in1=32'b0; in2=32'b0; address=2'b00; #1 // Set inputs and address, wait for new update
+	$display("%b  %b  %b|  %b  |  %b | 1", in0, in1, in2, address, out);
+	in0=32'b0; in1=32'b1; in2=32'b0; address=2'b01; #1
+	$display("%b  %b  %b|  %b  |  %b | 1", in0, in1, in2, address, out);
+	in0=32'b0; in1=32'b0; in2=32'b1; address=2'b10; #1
+	$display("%b  %b  %b|  %b  |  %b | 1", in0, in1, in2, address, out);
+	end
 
-	// mux4to1 dut3(.out(out),
-	// 		.address(address),
-	// 		.in0(in0),
-	// 		.in1(in1),
-	// 		.in2(in2),
-	// 		.in3(in3));
+	mux4to1 dut3(.out(out),
+			.address(address),
+			.in0(in0),
+			.in1(in1),
+			.in2(in2),
+			.in3(in3));
 
-	// initial begin
-	// $display("Testing 4:1 Mux:");
-	// $display("in0  in1  in3  in4 | address | out | expected out"); // Prints header for truth table
-	// in0=32'b1; in1=32'b0; in2=32'b0; in3=32'b0; address=2'b00; #1 // Set inputs and address, wait for new update
-	// $display("%b  %b  %b  %b|  %b  |  %b | 1", in0, in1, in2, in3, address, out);
-	// in0=32'b0; in1=32'b1; in2=32'b0; in3=32'b0; address=2'b01; #1
-	// $display("%b  %b  %b  %b|  %b  |  %b | 1", in0, in1, in2, in3, address, out);
-	// in0=32'b0; in1=32'b0; in2=32'b1; in3=32'b0; address=2'b10; #1
-	// $display("%b  %b  %b  %b|  %b  |  %b | 1", in0, in1, in2, in3, address, out);
-	// in0=32'b0; in1=32'b0; in2=32'b0; in3=32'b1; address=2'b11; #1
-	// $display("%b  %b  %b  %b|  %b  |  %b | 1", in0, in1, in2, in3, address, out);
-	// end
+	initial begin
+	$display("Testing 4:1 Mux:");
+	$display("in0  in1  in3  in4 | address | out | expected out"); // Prints header for truth table
+	in0=32'b1; in1=32'b0; in2=32'b0; in3=32'b0; address=2'b00; #1 // Set inputs and address, wait for new update
+	$display("%b  %b  %b  %b|  %b  |  %b | 1", in0, in1, in2, in3, address, out);
+	in0=32'b0; in1=32'b1; in2=32'b0; in3=32'b0; address=2'b01; #1
+	$display("%b  %b  %b  %b|  %b  |  %b | 1", in0, in1, in2, in3, address, out);
+	in0=32'b0; in1=32'b0; in2=32'b1; in3=32'b0; address=2'b10; #1
+	$display("%b  %b  %b  %b|  %b  |  %b | 1", in0, in1, in2, in3, address, out);
+	in0=32'b0; in1=32'b0; in2=32'b0; in3=32'b1; address=2'b11; #1
+	$display("%b  %b  %b  %b|  %b  |  %b | 1", in0, in1, in2, in3, address, out);
+	end
 
 endmodule
